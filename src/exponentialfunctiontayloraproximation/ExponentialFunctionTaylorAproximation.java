607046194a7b2c49196a0d23255efbe79f3f0bd0
Audifaz <emilio.aguila@hotmail.com>
2019-01-17 17:00:54 -0600

    Prueba

---
+++ src/exponentialfunctiontayloraproximation/ExponentialFunctionTaylorAproximation.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exponentialfunctiontayloraproximation;
+
+/**
+ *
+ * @author maer
+ */
+import java.util.Scanner;
+
+public class ExponentialFunctionTaylorAproximation {
+
+
+    public static int factorial(int n){    
+        if((n==1)||(n<=0))
+            return 1;
+        else
+            return n*factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        double exp;
+        int i;
+        int aprox=0;
+        Scanner stream;
+        stream=new Scanner(System.in);
+        //
+        System.out.print("Ingrese la accuracy de la funcion exponencial: ");
+        aprox=stream.nextInt();
+        //
+        exp=0;
+        if(aprox>0){  
+
+            for(i=0;i<aprox;i++){
+            exp=exp+(1.0/factorial(i));
+            System.out.println(exp);
+            }//End for
+    
+            
+       }//End if
+                System.out.println("El resultado final es: "+exp);
+    }
+    
+}
